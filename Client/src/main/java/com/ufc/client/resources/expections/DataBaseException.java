928fdf413ed7bff97c93986ccfb732bf4a994ef0
João Pedro <37542212+jpdal98@users.noreply.github.com>
2021-09-25 18:02:47 -0300

    Delete Client directory

--- Client/src/main/java/com/ufc/client/resources/expections/DataBaseException.java
+++
@@ -1,9 +0,0 @@
-package com.ufc.client.resources.expections;
-
-public class DataBaseException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	
-	public DataBaseException(String msg) {
-		super(msg);
-	}
-}
